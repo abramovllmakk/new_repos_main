bb13644fec48d9cd76861641f07ce428da21462f
Leonid Abramov <abramovaana859@gmail.com>
2026-06-29 17:45:34 +0500

    feat: добавлены UoW для users, response, jobs

--- src/uows/response_uow.py
+++
@@ -1,7 +0,0 @@
-from bases.uows.generic_alchemy_uow import AlchemyAsyncGenericUOW
-from models.alchemy import Response
-from models.dto.response_dto import ResponseCreateDTO, ResponseDTO
-
-
-class ResponseUOW(AlchemyAsyncGenericUOW[Response, ResponseDTO, ResponseCreateDTO, ResponseDTO]):
-    pass
